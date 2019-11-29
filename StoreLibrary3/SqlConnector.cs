ab594bdd9ccf26ae30a3bd26fc3d002f7249f6cc
Talitha <57577168+anidiotbuilds@users.noreply.github.com>
2019-11-29 11:17:15 -0800

    Add files via upload
    SqlConnector class connects to the database

---
+++ StoreLibrary3/SqlConnector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreLibrary3
+{
+    public class SqlConnector : IDataConnection
+    {
+        // TODO - make the AddCustomer method actually save to the database. 
+        /// <summary>
+        /// Saves a new customer to the database.
+        /// </summary>
+        /// <param name="model">The customer information.</param>
+        /// <returns>The customer information, incl. the unique identifier.</returns>
+        public CustomerModel AddCustomer(CustomerModel model)
+        {
+            model.Id = 1;
+
+            return model; 
+        }
+    }
+}
