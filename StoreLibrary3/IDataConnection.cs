734a984c9c2cd82318b2ec2f486c9a08f4adfba0
Talitha <57577168+anidiotbuilds@users.noreply.github.com>
2019-11-29 11:12:32 -0800

    Add files via upload
    Connection interface

---
+++ StoreLibrary3/IDataConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreLibrary3
+{
+    public interface IDataConnection
+    {
+        CustomerModel AddCustomer(CustomerModel model);
+    }
+}
