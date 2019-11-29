787b66430d4a3ede8c22b2cdd6fd32683675eeba
Talitha <57577168+anidiotbuilds@users.noreply.github.com>
2019-11-29 11:18:07 -0800

    Add files via upload
    TextConnector class connects to (optional) text file

---
+++ StoreLibrary3/TextConnector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreLibrary3
+{
+    public class TextConnector : IDataConnection
+    {
+        // TODO - Wire up the AddCustomer for text files
+        public CustomerModel AddCustomer(CustomerModel model)
+        {
+            model.Id = 1;
+
+            return model;
+        }
+    }
+}
