2c5e11ff3a1eff0907767625191ec2d1496cf759
garret-gomez <57699001+garret-gomez@users.noreply.github.com>
2019-12-01 16:30:18 -0800

    inserted into row 19-39

--- StoreUI/CustomerLoginForm.cs
+++ StoreUI/CustomerLoginForm.cs
@@ -15,6 +15,27 @@ namespace StoreUI
         public CustomerLoginForm()
         {
             InitializeComponent();
+        } 
+         //after clicking login button
+        {
+            //variables
+            string email, password; 
+            //input data
+            email = emailLoginTextBox.Text;
+            password = passwordLoginTextBox.Text; 
+            //checks input data 
+            if (email == "")
+            {
+                loginEmailLabel.Text = string.Format("Please enter an email");
+            } 
+            else if(password == "")
+            {
+                passwordLoginLabel.Text = string.Format("Please enter a password");
+            }
+            else if(password != " customers password")
+            {
+                passwordLoginLabel.Text = string.Format("Please enter correct password");
+            }
         }
     }
 }
